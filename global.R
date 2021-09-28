2c36dfad96f600281409451f0d957c5917793ce8
julien <marchadier.julien@gmail.com>
2021-09-28 14:14:13 +0200

    28

--- global.R
+++ global.R
@@ -1,9 +1,5 @@
-library(GGally)
-library(tidyverse)
-library(ggplot2)
-library(tibble)# un package tidyverse nécessaire
-library(dplyr)# un autre package tidyverse nécessaire
-library(shiny)
-library(gapminder)
+source("./packages.R")
+
 #https://www.kaggle.com/amritvirsinghx/web-series-ultimate-edition
 
+#a
---
+++ packages.R
@@ -0,0 +1,7 @@
+library(GGally)
+library(tidyverse)
+library(ggplot2)
+library(tibble)# un package tidyverse nécessaire
+library(dplyr)# un autre package tidyverse nécessaire
+library(shiny)
+library(gapminder)
