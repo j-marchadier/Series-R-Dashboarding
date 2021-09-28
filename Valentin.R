43645aef1e0b7a107f1e2bb9151f86e7c9ca5ef8
VRebuffey <valentin.rebuffey@edu.esiee.fr>
2021-09-28 18:35:35 +0200

    Update globale sans Pipe

--- Valentin.R
+++ Valentin.R
@@ -5,5 +5,24 @@ data=as_tibble(data)
 data=separate(data,Genre,c("genre_1","genre_2"),",",TRUE)
 #Il y avait des années dans la catégorie genre, on les remplace donc par NA car elles n'avaient aucune raison d'être ici.
 data=mutate(data,genre_2=ifelse(substr(genre_2,1,1)=='1' |substr(genre_2,1,1)=='2',NA,genre_2))
+class(data)
 view(head(data))
 
+data=separate(data,'Streaming Platform',c("streaming_platform1","streaming_platform2","streaming_platform3"),",",TRUE)
+
+df_non_na=filter(data$streaming_platform2,!if_any(everything(),is.na))
+view(df_non_na)
+typeof(data$streaming_platform3)
+
+
+
+
+# essais <- data %>%
+#   rename("Streaming_plateform" = "Streaming Platform") %>%
+#   mutate(nb_plateforme = length(str_split(data$`Streaming Platform`,",")[[1]]))
+
+# essais$nb_plateforme
+# str_split(data$`Streaming Platform`[3],",")
+# length(str_split(data$`Streaming Platform`[15],",")[[1]])
+# view(head(essais))
+# length(str_split(data$`Streaming Platform`[3], ',')[[1]])
--- global.R
+++ global.R
@@ -1,1 +1,19 @@
+#Loading the packages we need
 source("./packages.R")
+#Reading the csv file and setting types
+data <- read_csv("All_Streaming_Shows.csv", 
+                 col_names = c("title","year","content_rating","imdb_rating","r_rating","genre","description","nb_seasons","streaming_platform"),
+                 col_types = "cffdifccf",
+                 skip =1)
+#We at first clean the dataset :
+#Our first step is to change the type of the number of seasons into an integer and delete all the characters
+
+data$nb_seasons <- str_replace(str_replace(str_replace(data$nb_seasons,"Seasons",""),"Season","")," ","")
+data$nb_seasons <- as.integer(data$nb_seasons)
+#For the column Genre, we split it into two different columns since each serie can have 2 differents Genres
+data=separate(data,genre,c("genre_1","genre_2"),",",TRUE)
+#Since there's some years in the genre category, we decided to replace them with NAN values since a year isn't a genre
+data=mutate(data,genre_2=ifelse(substr(genre_2,1,1)=='1' |substr(genre_2,1,1)=='2',NA,genre_2))
+#We split the streaming platform into 3 different columns aswell since each serie can be broadcast on different platforms
+data=separate(data,'streaming_platform',c("streaming_platform1","streaming_platform2","streaming_platform3"),",",TRUE)
+view(data)
