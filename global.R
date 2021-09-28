0209375a76350c04c6c29a287a51113072337ef1
julien <marchadier.julien@gmail.com>
2021-09-28 14:05:33 +0200

    Update global.R

--- global.R
+++ global.R
@@ -5,3 +5,5 @@ library(tibble)# un package tidyverse nécessaire
 library(dplyr)# un autre package tidyverse nécessaire
 library(shiny)
 library(gapminder)
+
+#https://www.kaggle.com/amritvirsinghx/web-series-ultimate-edition
