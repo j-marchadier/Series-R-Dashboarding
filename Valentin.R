195be3c275e240955771e7a1bb8282f2cc0538c6
VRebuffey <valentin.rebuffey@edu.esiee.fr>
2021-10-04 11:58:41 +0200

    Update Valentin.R

--- Valentin.R
+++ Valentin.R
@@ -1,21 +1,27 @@
 source("./packages.R")
-data <- read_csv("All_Streaming_Shows.csv")
+data <- read_csv("Web_series_data.csv")
 data=as_tibble(data)
+
+data <- read_csv("Web_series_data.csv", 
+                 col_names = c("title","genre","tags","languages","series_ormouvies","hidden_gem_score",
+                               "country_availability","run_time","director","writer","actors",
+                               "view_rating","imdb_scrore","rotten_tomatoes_score","metacritic_score",
+                               "awards_received","awards_nominated","box_office","release_date",
+                               "production_house","netflix_link","imdb_link",'summary',"imdb_vote","image",
+                               "poster","tmdb_trailer","trailer_site"),
+                 col_types = "cffffdcccccccddddccccccdcccc",
+                 skip =1)
 #On split ici la colonne Genre en deux colonnes distinctes genre_1 et genre_2
-data=separate(data,Genre,c("genre_1","genre_2"),",",TRUE)
+data=separate(data,Genre,c("genre_1","genre_2","genre_3","genre_4","genre_5","genre_6"),",",TRUE)
 #Il y avait des années dans la catégorie genre, on les remplace donc par NA car elles n'avaient aucune raison d'être ici.
-data=mutate(data,genre_2=ifelse(substr(genre_2,1,1)=='1' |substr(genre_2,1,1)=='2',NA,genre_2))
-class(data)
+#data=mutate(data,genre_2=ifelse(substr(genre_2,1,1)=='1' |substr(genre_2,1,1)=='2',NA,genre_2))
+#class(data)
+#view(head(data))
+data=separate(data,Languages,c("language_1","language_2","language_3","language_4","language_5","language_6","language_7"),",",TRUE)
+data=mutate(data,release_year=substr(release_date,8,12))
+#data=separate(data,'Streaming Platform',c("streaming_platform1","streaming_platform2","streaming_platform3"),",",TRUE)
+#typeof(data$genre_2)
 view(head(data))
-
-data=separate(data,'Streaming Platform',c("streaming_platform1","streaming_platform2","streaming_platform3"),",",TRUE)
-
-df_non_na=filter(data$streaming_platform2,!if_any(everything(),is.na))
-view(df_non_na)
-typeof(data$streaming_platform3)
-
-
-
 
 # essais <- data %>%
 #   rename("Streaming_plateform" = "Streaming Platform") %>%
