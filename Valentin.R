8c7edf858c5ff8c9fc5e809bfa41e58afdf68795
VRebuffey <valentin.rebuffey@edu.esiee.fr>
2021-10-04 12:26:42 +0200

    Global fonctionnel

--- Valentin.R
+++ Valentin.R
@@ -1,27 +1,8 @@
-source("./packages.R")
-data <- read_csv("Web_series_data.csv")
-data=as_tibble(data)
-
-data <- read_csv("Web_series_data.csv", 
-                 col_names = c("title","genre","tags","languages","series_ormouvies","hidden_gem_score",
-                               "country_availability","run_time","director","writer","actors",
-                               "view_rating","imdb_scrore","rotten_tomatoes_score","metacritic_score",
-                               "awards_received","awards_nominated","box_office","release_date",
-                               "production_house","netflix_link","imdb_link",'summary',"imdb_vote","image",
-                               "poster","tmdb_trailer","trailer_site"),
-                 col_types = "cffffdcccccccddddccccccdcccc",
-                 skip =1)
-#On split ici la colonne Genre en deux colonnes distinctes genre_1 et genre_2
-data=separate(data,Genre,c("genre_1","genre_2","genre_3","genre_4","genre_5","genre_6"),",",TRUE)
 #Il y avait des années dans la catégorie genre, on les remplace donc par NA car elles n'avaient aucune raison d'être ici.
 #data=mutate(data,genre_2=ifelse(substr(genre_2,1,1)=='1' |substr(genre_2,1,1)=='2',NA,genre_2))
-#class(data)
-#view(head(data))
-data=separate(data,Languages,c("language_1","language_2","language_3","language_4","language_5","language_6","language_7"),",",TRUE)
-data=mutate(data,release_year=substr(release_date,8,12))
+
 #data=separate(data,'Streaming Platform',c("streaming_platform1","streaming_platform2","streaming_platform3"),",",TRUE)
 #typeof(data$genre_2)
-view(head(data))
 
 # essais <- data %>%
 #   rename("Streaming_plateform" = "Streaming Platform") %>%
@@ -32,3 +13,34 @@ view(head(data))
 # length(str_split(data$`Streaming Platform`[15],",")[[1]])
 # view(head(essais))
 # length(str_split(data$`Streaming Platform`[3], ',')[[1]])
+
+
+source("./packages.R")
+data <- read_csv("Web_series_data.csv")
+data=as_tibble(data)
+
+df<- read_csv("Web_series_data.csv", 
+              col_names = c("title","genre","tags","languages","series_ormouvies","hidden_gem_score",
+                            "country_availability","run_time","director","writer","actors",
+                            "view_rating","imdb_scrore","rotten_tomatoes_score","metacritic_score",
+                            "awards_received","awards_nominated","box_office","release_date","netflix_date",
+                            "production_house","netflix_link","imdb_link",'summary',"imdb_vote","image",
+                            "poster","tmdb_trailer","trailer_site"),
+              col_types = "cffffdcccccccddddcccccccdcccc",
+              skip =1)
+data = select(df , -c(tags,languages,actors,view_rating,rotten_tomatoes_score,metacritic_score,production_house,netflix_link,imdb_link,tmdb_trailer,trailer_site))
+#On split ici la colonne Genre en deux colonnes distinctes genre_1 et genre_2
+#data=separate(data,genre,c("genre_1","genre_2","genre_3","genre_4","genre_5","genre_6"),",",TRUE)
+
+#data=separate(data,languages,c("language_1","language_2","language_3","language_4","language_5","language_6","language_7"),",",TRUE)
+#On transforme la date de sortie pour ne garder que l'année
+data=mutate(data,release_date=substr(release_date,8,12))
+colnames(data)[13] <- "release_year"
+#On transforme la date de sortie sur Netflix pour ne garder que l'année
+data=mutate(data,netflix_date=substr(netflix_date,1,4))
+colnames(data)[14] <- "release_netflix_year"
+#On remove le Dollar de boxoffice
+data=mutate(data,box_office=substr(box_office,2,15))
+view(head(data))
+
+
--- global.R
+++ global.R
@@ -4,24 +4,33 @@ source("./packages.R")
 
 #Reading the csv file and setting types
 df <- read_csv("Web_series_data.csv", 
-                 col_names = c("title","genre","tags","languages","series_ormouvies","hidden_gem_score",
+                 col_names = c("title","genre","tags","languages","series_or_mouvies","hidden_gem_score",
                                "country_availability","run_time","director","writer","actors",
-                               "view_rating","imdb_scrore","rotten_tomatoes_score","metacritic_score",
-                               "awards_received","awards_nominated","box_office","release_date",
+                               "view_rating","imdb_score","rotten_tomatoes_score","metacritic_score",
+                               "awards_received","awards_nominated","box_office","release_date","netflix_release_date",
                                "production_house","netflix_link","imdb_link",'summary',"imdb_vote","image",
                                "poster","tmdb_trailer","trailer_site"),
                  col_types = "cffffdcccccccddddccccccdcccc",
                  skip =1)
-
+view(df)
 data = df %>% select(-c(tags,languages,actors,view_rating,rotten_tomatoes_score,metacritic_score,production_house,netflix_link,imdb_link,tmdb_trailer,trailer_site))
-
+view(data)
 #We at first clean the dataset :
 #Our first step is to change the type of the number of seasons into an integer and delete all the characters
-data <- data %>% separate(genre,c("genre_1","genre_2","genre_3","genre_4","genre_5","genre_6"),",",TRUE)
+
 
 #data$languages <- data$languages %>% separate(c("language_1","language_2","language_3","language_4","language_5","language_6","language_7"),",",TRUE)
+#On transforme la date de sortie pour ne garder que l'année
+data = data %>% mutate(release_date=substr(release_date,8,12))
+colnames(data)[13] <- "release_year"
+#On transforme la date de sortie sur Netflix pour ne garder que l'année
+data = data %>% mutate(data,netflix_release_date=substr(netflix_release_date,1,4))
+colnames(data)[14] <- "release_netflix_year"
+#On remove le Dollar de boxoffice
+data= data %>% mutate(data,box_office=substr(box_office,2,15))
+view(head(data))
 
-data = data %>% mutate(release_year=substr(release_date,8,12))
+data <- data %>% separate(genre,c("genre_1","genre_2","genre_3","genre_4","genre_5","genre_6"),",",TRUE)
 view(data)
 
 #data$nb_seasons <- str_replace(str_replace(str_replace(data$nb_seasons,"Seasons",""),"Season","")," ","")
