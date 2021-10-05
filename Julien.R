4dad3e9d76a36f2d5e86de8d45ff4d69edf2c9b6
julien <marchadier.julien@gmail.com>
2021-10-05 13:32:30 +0200

    Update Julien.R

--- Julien.R
+++ Julien.R
@@ -18,11 +18,12 @@ df<- read_csv("Web_series_data.csv",
 #Our first step is to change the type of the number of seasons into an integer and delete all the characters
 data = df %>% select(-c(tags,languages,actors,view_rating,rotten_tomatoes_score,metacritic_score,production_house,netflix_link,imdb_link,tmdb_trailer,trailer_site))
 
-#For the column Genre, we split it into two different columns since each serie can have 2 differents Genres
-data <- data %>% separate(genre,c("genre_1","genre_2","genre_3","genre_4","genre_5","genre_6"),",",TRUE)
-
 #Since there's some years in the genre category, we decided to replace them with NAN values since a year isn't a genre
 data=mutate(data,genre_2=ifelse(substr(genre_2,1,1)=='1' |substr(genre_2,1,1)=='2',NA,genre_2))
+
+#For the column Genre, we split it into two different columns since each serie can have 2 differents Genres
+data_genres <- data %>% separate(genre,c("genre_1","genre_2","genre_3","genre_4","genre_5","genre_6"),",",TRUE)
+
 
 view(head(data))
 
