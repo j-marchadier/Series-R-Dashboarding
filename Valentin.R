21ecf7d9cffff911aef57c6d2e0bf178e66ba5de
VRebuffey <valentin.rebuffey@edu.esiee.fr>
2021-10-05 16:10:48 +0200

    Genre Fini + global corrigé

--- Valentin.R
+++ Valentin.R
@@ -28,19 +28,40 @@ df<- read_csv("Web_series_data.csv",
                             "poster","tmdb_trailer","trailer_site"),
               col_types = "cffffdcccccccddddcccccccdcccc",
               skip =1)
-data = select(df , -c(tags,languages,actors,view_rating,rotten_tomatoes_score,metacritic_score,production_house,netflix_link,imdb_link,tmdb_trailer,trailer_site))
-#On split ici la colonne Genre en deux colonnes distinctes genre_1 et genre_2
-#data=separate(data,genre,c("genre_1","genre_2","genre_3","genre_4","genre_5","genre_6"),",",TRUE)
+data = df %>% select(-c(tags,languages,actors,view_rating,rotten_tomatoes_score,metacritic_score,production_house,netflix_link,imdb_link,tmdb_trailer,trailer_site))
+#We at first clean the dataset :
+#Our first step is to change the type of the number of seasons into an integer and delete all the characters
 
-#data=separate(data,languages,c("language_1","language_2","language_3","language_4","language_5","language_6","language_7"),",",TRUE)
+
+#data$languages <- data$languages %>% separate(c("language_1","language_2","language_3","language_4","language_5","language_6","language_7"),",",TRUE)
 #On transforme la date de sortie pour ne garder que l'année
-data=mutate(data,release_date=substr(release_date,8,12))
+data = data %>% mutate(release_date=substr(release_date,8,12))
 colnames(data)[13] <- "release_year"
 #On transforme la date de sortie sur Netflix pour ne garder que l'année
-data=mutate(data,netflix_date=substr(netflix_date,1,4))
+data = data %>% mutate(data,netflix_date=substr(netflix_date,1,4))
 colnames(data)[14] <- "release_netflix_year"
 #On remove le Dollar de boxoffice
-data=mutate(data,box_office=substr(box_office,2,15))
-view(head(data))
+data= data %>% mutate(data,box_office=substr(box_office,2,15))
+#data$genre %>% separate(genre)
+#unique(data$genre)
+data_genre_sep <- data %>% separate(genre,c("genre_1","genre_2","genre_3","genre_4","genre_5","genre_6"),", ",TRUE)
+#view(head(data))
+#vecteur pays avec tous les pays
+#titre année genre table
+#créer matrice remplie de 0 avec en colonne le nom des genres
+#parcours la table si le genre est dans unique
+vecteur_genre=unique(c(data_genre_sep$genre_1,data_genre_sep$genre_2,data_genre_sep$genre_3,data_genre_sep$genre_4,data_genre_sep$genre_5,data_genre_sep$genre_6))
+vecteur_genre=vecteur_genre[vecteur_genre!=""]
+vecteur_genre=head(vecteur_genre,-1)
 
+data_genre_availability = data
+for (test in vecteur_genre){
+  print(test)
+  v <- data_genre_availability %>% 
+    add_column(i = if_else(grepl(test, data$genre, fixed=TRUE), 
+                           TRUE, FALSE)) %>%
+    dplyr::select(i)
+  data_genre_availability[, test] <- v
+}
+view(head(data_genre_availability))
 
--- global.R
+++ global.R
@@ -12,9 +12,7 @@ df<- read_csv("Web_series_data.csv",
                             "poster","tmdb_trailer","trailer_site"),
               col_types = "cffffdcccccccddddcccccccdcccc",
               skip =1)
-view(head(df))
 data = df %>% select(-c(tags,languages,actors,view_rating,rotten_tomatoes_score,metacritic_score,production_house,netflix_link,imdb_link,tmdb_trailer,trailer_site))
-view(head(data))
 #We at first clean the dataset :
 #Our first step is to change the type of the number of seasons into an integer and delete all the characters
 
